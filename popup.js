56d4819face80196f04379f852a4de98cbbe39af
joaqu1m <joaquim.pires@sptech.school>
2024-05-30 14:03:00 -0300

    init

---
+++ popup.js
@@ -0,0 +1,6 @@
+// Como pegar as abas do browser
+// document.getElementById("teste").addEventListener("click", () => {
+//   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+//     console.log(tabs);
+//   });
+// });
